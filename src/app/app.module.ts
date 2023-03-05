77cbb5f3e3d3a195a082a7399403536098d29dd8
Rachid <chakor.rachid.1@gmail.com>
2023-03-05 15:05:00 -0500

    created homeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule
   ],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../models/todo.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  todos: Todo[] = [
+    { text: 'text1' },
+    { text: 'text2' },
+    { text: 'text3' },
+    { text: 'text4' }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 
 }
